86a76ad63af08440b4dfe274930f2a1cc3a9f7a0
Samir Khan <samirkhan@Samirs-MacBook-Air.local>
2026-06-20 13:38:10 +0530

    chore: update next.config.ts basePath and Dockerfile api url for production deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   output: "standalone",
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  basePath: "/groww-insights",
 };
 
 export default nextConfig;
